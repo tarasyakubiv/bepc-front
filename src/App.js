5e5702403413b4d9b3691d71de390a5f592b3bb7
Taras Yakubiv <taras.yakubiv@gmail.com>
2019-01-19 19:37:45 -0600

    Changed title/icon

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import Tree from './components/Tree';
+import {Helmet} from "react-helmet";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Helmet>
+          <title>Factory Tree</title>
+          <link rel="icon" type="image/png" href="factory.png" sizes="16x16" />
+        </Helmet>
         <Tree />
       </div>
     );
